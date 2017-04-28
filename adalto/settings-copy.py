3a548a52ba34881ba0594a34cd0bcf977cf34069
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-04-27 22:06:01 -0300

    mysql

--- adalto/settings-copy.py
+++ adalto/settings-copy.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'adalto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_adalto',
-        'USER': 'adaltopicotti',
-        'PASSWORD': '#Shindemiro0',
-        'HOST': 'adaltopicotti.mysql.pythonanywhere-services.com',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
