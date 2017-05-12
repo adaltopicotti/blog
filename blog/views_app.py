dd670c3bb31914b3653be230c7aec74fe61cb3fb
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-05-12 15:53:15 -0300

    Update views_app.py

--- blog/views_app.py
+++ blog/views_app.py
@@ -145,10 +145,10 @@ def calc_coord(lat):
     #sec = float(laSec)
     dec = (((float(lat[2])/60) + float(lat[1]))/60)
     if opt == 'opt1':
-        lat_dec = -(lat[0] + dec)
+        lat_dec = -(float(lat[0]) + dec)
     elif opt == 'opt2':
-        lat_dec = (lat[0] + dec)
-    return repr(lat_dec)
+        lat_dec = (float(lat[0]) + dec)
+    return lat_dec
 
 def calcLon(opt, loDeg, loMin, loSec):
 
