4173dc4bbc677730b64c5d3214f382433d9103a1
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-06-09 11:45:19 -0300

    Create views_app.py

--- blog/views_app.py
+++ blog/views_app.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 import json, requests
+from .models import Post, Comment
 from django.http import JsonResponse, HttpResponse
 
 
