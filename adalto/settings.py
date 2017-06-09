728ade2088103aa95ac9d24090dec825bf2a3539
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-06-09 09:24:35 -0300

    Create settings.py

--- adalto/settings.py
+++ adalto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_)(5!i3fxo%(71r+26yiymxuri_*^-%onfnqfejr9zf!mpa32z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
