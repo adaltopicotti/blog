7e7a3d4827ed4b80606efbdc12e886fb9c56442b
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-06-22 16:07:06 -0300

    Update views_app.py

--- blog/views_app.py
+++ blog/views_app.py
@@ -140,7 +140,6 @@ def coordinate(request):
             url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ repr(coord[0]) +','+ repr(coord[1]) +'&key=AIzaSyA_3wK_DfiwW94-1dg352-I8Zs__FGYrDo'
             result = requests.get(url)
             geoJson = result.json()
-            weather = get_wheater(str(coord[0]),str(coord[1]))
             return render(request, 'application/coordinate.html', {
                 'lat': coord[0],
                 'lon': coord[1],
