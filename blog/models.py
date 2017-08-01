112d244e13a14d3452414ca81e8890d17f9e4666
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-08-01 15:29:47 -0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -35,7 +35,8 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
     
-class = ForLogic(models.Model):
+  
+class ForLogic(models.Model):
 nome = models.CharField(max_length=200)
 email = models.EmailField()
 register_date = models.DateTimeField(default=timezone.now)
