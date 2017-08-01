5d5fd99c685cccaf025a59b7d48f5fe9892e066a
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-08-01 14:59:32 -0300

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post, Comment, ForLogic
 
 class PostForm(forms.ModelForm):
 
@@ -11,3 +11,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text',)
+  
+
+class Register_4logic(forms.ModelForm):
+    class Meta:
+        model = ForLogic
+        fields = ('nome', 'email',)
+
