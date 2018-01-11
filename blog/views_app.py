567a4faebf225c8a511900f896e5c7dcf627ea8b
Adalto Picotti Junior <picotti.junior@gmail.com>
2018-01-11 09:59:00 -0200

    Update views_app.py

--- blog/views_app.py
+++ blog/views_app.py
@@ -29,7 +29,8 @@ lonInfo = """
 def subv_est(request):
     if request.method == 'POST':
         subv_status = request.POST['subv_status']
-        return render(request, 'app/subv_est.html', {'ative': subv_status})
+		cotadores = {'cotador':['pr', 'go']}
+        return render(request, 'app/subv_est.html', {'ative': subv_status, 'cotadores': cotadores})
     return render(request, 'app/subv_est.html', {})
 
 
