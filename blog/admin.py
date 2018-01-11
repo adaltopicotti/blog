715dc810540a59b27afe48dc70ee9de6537a9dcc
Adalto Picotti Junior <picotti.junior@gmail.com>
2018-01-11 11:08:00 -0200

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment, ForLogic
+from .models import *
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(ForLogic)
+admin.site.register(Cotador)
